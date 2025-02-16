d4db164d1fd24e480a21ab2499385b5e8d395588
Jonathan Meffert <jlmeffert@yahoo.com>
2025-02-15 23:02:11 -0500

    Add Cors Fix file references for live server

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+//budget api
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
 // Add require for file system
 const fs = require('fs');
+
+app.use(cors());
 // Load budget data from JSON file
 const budget = JSON.parse(fs.readFileSync('budgetData.json', 'utf8'));
 
-app.use('/', express.static('public'));
+
 
 app.get("/hello", (req, res) => {
     res.send("Hello World");
